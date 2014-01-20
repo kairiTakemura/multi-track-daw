01c39dded286af93d39cdbc8942b47d59358e0c3
kazuhide maeda <kzhd15med3g@gmail.com>
2014-01-20 23:09:28 +0900

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiTrackDaw::Application.config.secret_key_base = 'ca40512e0001c290e913b8ef59207f771eca144d4f6235c633c6723c81d26059fd43629f1dc78bb29f5e1d7a11e5f6e6932763db238dc5db405c609980c47c34'
