ea49e596af129fd30e5f10fffb2d17a1a567456d
kazuhide maeda <kzhd15med3g@gmail.com>
2014-01-24 15:45:18 +0900

    remove daw

--- app/views/daws/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@daws) do |daw|
-  json.extract! daw, :id
-  json.url daw_url(daw, format: :json)
-end
--- spec/models/daw_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Daw do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
